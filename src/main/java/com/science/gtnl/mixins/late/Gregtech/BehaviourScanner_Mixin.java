2ec50784f255792349bf837aa140f22b35e9b22d
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-06-07 03:05:52 +0800

    添加扫描仪显示无线蒸汽电网数量的功能

--- src/main/java/com/science/gtnl/mixins/late/Gregtech/BehaviourScanner_Mixin.java
+++ src/main/java/com/science/gtnl/mixins/late/Gregtech/BehaviourScanner_Mixin.java
@@ -1,12 +1,16 @@
 package com.science.gtnl.mixins.late.Gregtech;
 
+import static com.science.gtnl.Utils.steam.SteamWirelessNetworkManager.getUserSteam;
+
 import java.util.ArrayList;
+import java.util.UUID;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -16,6 +20,8 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 import com.science.gtnl.Utils.enums.Mods;
 import com.science.gtnl.Utils.recipes.ChanceBonusManager;
+import com.science.gtnl.common.machine.hatch.WirelessSteamDynamoHatch;
+import com.science.gtnl.common.machine.hatch.WirelessSteamEnergyHatch;
 import com.science.gtnl.config.MainConfig;
 
 import gregtech.api.enums.SoundResource;
@@ -25,6 +31,7 @@ import gregtech.api.metatileentity.implementations.MTEMultiBlockBase;
 import gregtech.api.util.GTRecipe;
 import gregtech.api.util.GTUtility;
 import gregtech.common.items.behaviors.BehaviourScanner;
+import gregtech.common.misc.spaceprojects.SpaceProjectManager;
 
 @SuppressWarnings("UnusedMixin")
 @Mixin(value = BehaviourScanner.class, remap = false)
@@ -44,11 +51,41 @@ public class BehaviourScanner_Mixin {
                 aStack,
                 GTUtility.getCoordinateScan(tList, aPlayer, aWorld, 1, aX, aY, aZ, side, hitX, hitY, hitZ),
                 aPlayer)) {
+                TileEntity tile = aWorld.getTileEntity(aX, aY, aZ);
+                if (tile instanceof BaseMetaTileEntity baseMetaTileEntity) {
+                    Object meta = baseMetaTileEntity.getMetaTileEntity();
+                    if (meta instanceof WirelessSteamDynamoHatch || meta instanceof WirelessSteamEnergyHatch) {
+                        String username = aPlayer.getCommandSenderName();
+                        String formatted_username = EnumChatFormatting.BLUE + username + EnumChatFormatting.RESET;
+                        UUID userUUID = SpaceProjectManager.getPlayerUUIDFromName(username);
+                        UUID teamUUID = SpaceProjectManager.getLeader(userUUID);
 
-                if (!Mods.Overpowered.isModLoaded() && MainConfig.enableRecipeOutputChance) {
-                    TileEntity tile = aWorld.getTileEntity(aX, aY, aZ);
-                    if (tile instanceof BaseMetaTileEntity baseMetaTile) {
-                        Object meta = baseMetaTile.getMetaTileEntity();
+                        if (!SpaceProjectManager.isInTeam(userUUID)) {
+                            String notInNetwork = String
+                                .format(StatCollector.translateToLocal("Info_SteamNetwork_00"), formatted_username);
+                            tList.add(notInNetwork);
+                        } else {
+                            String steamInfo = String.format(
+                                StatCollector.translateToLocal("Info_SteamNetwork_01"),
+                                formatted_username,
+                                EnumChatFormatting.RED,
+                                GTUtility.formatNumbers(getUserSteam(userUUID)),
+                                EnumChatFormatting.RESET);
+                            tList.add(steamInfo);
+
+                            if (!userUUID.equals(teamUUID)) {
+                                String networkInfo = String.format(
+                                    StatCollector.translateToLocal("Info_SteamNetwork_02"),
+                                    formatted_username,
+                                    EnumChatFormatting.BLUE,
+                                    SpaceProjectManager.getPlayerNameFromUUID(teamUUID),
+                                    EnumChatFormatting.RESET);
+                                tList.add(networkInfo);
+                            }
+                        }
+                    }
+
+                    if (!Mods.Overpowered.isModLoaded() && MainConfig.enableRecipeOutputChance) {
                         if (meta instanceof MTEMultiBlockBase mte) {
                             GTRecipe recipe = ChanceBonusManager.customProvider.getRecipeForMachine(mte);
                             if (recipe != null) {
@@ -65,6 +102,7 @@ public class BehaviourScanner_Mixin {
                                 tList.add(debugMessage);
                             }
                         }
+
                     }
                 }
 
