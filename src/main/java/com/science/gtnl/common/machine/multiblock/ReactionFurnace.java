40f938be91eb36aec71de18f793ccbfceaad5e8b
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-08-31 21:38:41 +0800

    修复一个耗电问题

--- src/main/java/com/science/gtnl/common/machine/multiblock/ReactionFurnace.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/ReactionFurnace.java
@@ -3,6 +3,7 @@ package com.science.gtnl.common.machine.multiblock;
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.*;
 import static com.science.gtnl.ScienceNotLeisure.RESOURCE_ROOT_ID;
 import static gregtech.api.GregTechAPI.*;
+import static gregtech.api.enums.GTValues.*;
 import static gregtech.api.enums.HatchElement.*;
 import static gregtech.api.util.GTStructureUtility.buildHatchAdder;
 import static gregtech.api.util.GTUtility.validMTEList;
