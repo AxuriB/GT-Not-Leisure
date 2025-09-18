a98e9053a4604e74fa3edb0a1507ac804b52cfbd
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-09-18 13:41:51 +0800

    Update MixinGodForgeMath.java

--- src/main/java/com/science/gtnl/mixins/late/Gregtech/MixinGodForgeMath.java
+++ src/main/java/com/science/gtnl/mixins/late/Gregtech/MixinGodForgeMath.java
@@ -5,7 +5,9 @@ import static tectech.thing.metaTileEntity.multi.godforge.upgrade.ForgeOfGodsUpg
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.science.gtnl.common.machine.multiblock.FOGAlloyBlastSmelterModule;
 import com.science.gtnl.common.machine.multiblock.FOGAlloySmelterModule;
@@ -30,4 +32,25 @@ public abstract class MixinGodForgeMath {
             cir.setReturnValue(true);
         }
     }
+
+    @Inject(
+        method = "calculateMaxParallelForModules(Ltectech/thing/metaTileEntity/multi/godforge/MTEBaseModule;Ltectech/thing/metaTileEntity/multi/godforge/MTEForgeOfGods;I)V",
+        at = @At("TAIL"),
+        locals = LocalCapture.CAPTURE_FAILSOFT)
+    private static void onCalculateMaxParallelForModulesTail(MTEBaseModule module, MTEForgeOfGods godforge,
+        int fuelFactor, CallbackInfo ci, int baseParallel, float fuelFactorMultiplier, float heatMultiplier,
+        float upgradeAmountMultiplier, int node53, boolean isMoltenOrSmeltingWithUpgrade, float totalBonuses,
+        int maxParallel) {
+        if (module instanceof FOGAlloySmelterModule) {
+            baseParallel = 4096;
+            module.setMaxParallel((int) (baseParallel * totalBonuses));
+        } else if (module instanceof FOGExtractorModule) {
+            baseParallel = 2048;
+            module.setMaxParallel((int) (baseParallel * totalBonuses));
+        } else if (module instanceof FOGAlloyBlastSmelterModule) {
+            baseParallel = 1024;
+            module.setMaxParallel((int) (baseParallel * totalBonuses));
+        }
+    }
+
 }
