b362656bbacbc078200f8b98811e4cac02ed1c57
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-09-28 20:53:53 +0800

    Update Sudoku.java

--- src/main/java/com/lootgames/sudoku/Sudoku.java
+++ src/main/java/com/lootgames/sudoku/Sudoku.java
@@ -9,11 +9,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.lootgames.sudoku.block.SudokuBlocks;
-import com.lootgames.sudoku.config.LGConfigs;
+import com.lootgames.sudoku.config.ConfigSudoku;
 import com.lootgames.sudoku.packet.GamePackets;
 import com.lootgames.sudoku.sudoku.GameSudoku;
 import com.lootgames.sudoku.sudoku.SudokuOverlayHandler;
-import com.science.gtnl.Utils.enums.Mods;
+import com.science.gtnl.Utils.enums.ModList;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -24,13 +24,15 @@ import ru.timeconqueror.lootgames.api.LootGamesAPI;
 @Mod(modid = MODID, version = "0.0.1", name = MODNAME, acceptedMinecraftVersions = "1.7.10")
 public class Sudoku {
 
-    @Mod.Instance(Mods.ModIds.SUDOKU)
+    public static ConfigSudoku SUDOKU = new ConfigSudoku();
+
+    @Mod.Instance(ModList.ModIds.SUDOKU)
     public static Sudoku instance;
-    public static final String MODID = Mods.ModIds.SUDOKU;
+    public static final String MODID = ModList.ModIds.SUDOKU;
     public static final String MODNAME = "LootGamesSudoku";
     public static final String VERSION = "0.0.1";
     public static final String Arthor = "HFstudio";
-    public static final String RESOURCE_ROOT_ID = Mods.ModIds.SUDOKU;
+    public static final String RESOURCE_ROOT_ID = ModList.ModIds.SUDOKU;
     public static final Logger LOG = LogManager.getLogger(MODID);
 
     @SidedProxy(clientSide = "com.lootgames.sudoku.ClientProxy", serverSide = "com.lootgames.sudoku.CommonProxy")
@@ -50,7 +52,7 @@ public class Sudoku {
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
         SudokuBlocks.register();
-        LGConfigs.load();
+        SUDOKU.load();
         MinecraftForge.EVENT_BUS.register(new SudokuOverlayHandler());
     }
 }
