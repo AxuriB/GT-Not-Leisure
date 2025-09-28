d1984cb4921629453b9d66980ff96fd39093806c
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-09-28 14:42:50 +0800

    修改艾萨的配方

--- src/main/java/com/science/gtnl/common/recipe/GTNL/IsaMillRecipes.java
+++ src/main/java/com/science/gtnl/common/recipe/GTNL/IsaMillRecipes.java
@@ -38,13 +38,21 @@ public class IsaMillRecipes implements IRecipePool {
         addIsaMillSet(Materials.Redstone);
         addIsaMillSet(Materials.NaquadahEnriched);
 
-        addIsaMillRecipeCustom(Materials.Netherrack, new ItemStack(Blocks.netherrack, 16), 1, 64, 100, 1, 2400);
+        addIsaMillRecipeCustom(Materials.Netherrack, new ItemStack(Blocks.netherrack, 16), 1, 96, 100, 1, 2400);
         addIsaMillRecipeCustom(Materials.Netherrack, new ItemStack(Blocks.netherrack, 16), 10, 64, 100, 2, 1200);
     }
 
     public void addIsaMillSet(Materials material) {
         Object[][] combos = { { OrePrefixes.ore, 1, 96, 100, 2, 4800 }, { OrePrefixes.ore, 10, 72, 100, 1, 2400 },
-            { OrePrefixes.rawOre, 1, 48, 50, 2, 2400 }, { OrePrefixes.rawOre, 10, 36, 50, 1, 1200 } };
+            { OrePrefixes.ore, 1, 96, 100, 2, 4800 }, { OrePrefixes.ore, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreNetherrack, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreNetherrack, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreEndstone, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreEndstone, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreBlackgranite, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreBlackgranite, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreRedgranite, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreRedgranite, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreMarble, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreMarble, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.oreBasalt, 1, 96, 100, 2, 4800 }, { OrePrefixes.oreBasalt, 10, 72, 100, 1, 2400 },
+            { OrePrefixes.rawOre, 1, 48, 50, 2, 2400 }, { OrePrefixes.rawOre, 10, 36, 50, 1, 1200 },
+            { OrePrefixes.crushed, 1, 48, 50, 2, 2400 }, { OrePrefixes.crushed, 10, 36, 50, 1, 1200 } };
 
         for (Object[] c : combos) {
             addIsaMillRecipe(material, (OrePrefixes) c[0], (int) c[1], (int) c[2], (int) c[3], (int) c[4], (int) c[5]);
