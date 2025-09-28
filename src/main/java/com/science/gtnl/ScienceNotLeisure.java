d20e80d5ab61ffc0f40ad2c42d1587ee4cf749a2
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-09-28 20:54:45 +0800

    Update ScienceNotLeisure.java

--- src/main/java/com/science/gtnl/ScienceNotLeisure.java
+++ src/main/java/com/science/gtnl/ScienceNotLeisure.java
@@ -6,22 +6,16 @@ import static com.science.gtnl.ScienceNotLeisure.MODNAME;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.science.gtnl.Utils.enums.Mods;
+import com.science.gtnl.Utils.enums.ModList;
 import com.science.gtnl.Utils.item.MissingMappingsHandler;
-import com.science.gtnl.Utils.text.LanguageManager;
-import com.science.gtnl.api.TickrateAPI;
 import com.science.gtnl.common.command.CommandEnergyNetwork;
 import com.science.gtnl.common.command.CommandPlaySound;
 import com.science.gtnl.common.command.CommandReloadConfig;
 import com.science.gtnl.common.command.CommandSteamNetwork;
+import com.science.gtnl.common.command.CommandSudo;
 import com.science.gtnl.common.command.CommandTickrate;
 import com.science.gtnl.common.command.CommandTitle;
-import com.science.gtnl.common.machine.multiMachineClasses.EdenGardenManager.EIGBucketLoader;
-import com.science.gtnl.config.MainConfig;
-import com.science.gtnl.loader.MachineLoader;
 import com.science.gtnl.loader.MaterialLoader;
-import com.science.gtnl.loader.RecipeLoaderRunnable;
-import com.science.gtnl.loader.ScriptLoader;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -57,13 +51,13 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
     acceptedMinecraftVersions = "1.7.10")
 public class ScienceNotLeisure {
 
-    @Mod.Instance(Mods.ModIds.SCIENCENOTLEISURE)
+    @Mod.Instance(ModList.ModIds.SCIENCENOTLEISURE)
     public static ScienceNotLeisure instance;
-    public static final String MODID = Mods.ModIds.SCIENCENOTLEISURE;
+    public static final String MODID = ModList.ModIds.SCIENCENOTLEISURE;
     public static final String MODNAME = "GTNotLeisure";
     public static final String VERSION = Tags.VERSION;
-    public static final String Arthor = "HFstudio";
-    public static final String RESOURCE_ROOT_ID = Mods.ModIds.SCIENCENOTLEISURE;
+    public static final String ARTHOR = "HFstudio";
+    public static final String RESOURCE_ROOT_ID = ModList.ModIds.SCIENCENOTLEISURE;
     public static final Logger LOG = LogManager.getLogger(MODID);
 
     public static SimpleNetworkWrapper network;
@@ -76,33 +70,29 @@ public class ScienceNotLeisure {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         network = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
+        NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
         proxy.preInit(event);
         MaterialLoader.loadPreInit();
-        LanguageManager.init();
-
-        new RecipeLoaderRunnable().run();
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
-        MachineLoader.registerGlasses();
-        TickrateAPI.changeTickrate(MainConfig.defaultTickrate);
+        MaterialLoader.loadInit();
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
-        EIGBucketLoader.LoadEIGBuckets();
         MaterialLoader.loadPostInit();
-        MachineLoader.run();
     }
 
     @Mod.EventHandler
     public void completeInit(FMLLoadCompleteEvent event) {
-        ScriptLoader.run();
+        proxy.completeInit(event);
+        MaterialLoader.loadCompleteInit();
     }
 
     // register server commands in this event handler (Remove if not needed)
@@ -114,6 +104,7 @@ public class ScienceNotLeisure {
         event.registerServerCommand(new CommandSteamNetwork());
         event.registerServerCommand(new CommandEnergyNetwork());
         event.registerServerCommand(new CommandPlaySound());
+        event.registerServerCommand(new CommandSudo());
     }
 
     @Mod.EventHandler
