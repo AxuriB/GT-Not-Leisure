b63d0895077c8e6c32d2509588e0c7e8a3ab9e3a
Psi <Psi@123.com>
2025-10-19 20:04:18 +0800

    修复蒸汽飞行模块buff时间不够导致坠机

--- src/main/java/com/science/gtnl/common/recipe/GTNL/ElectrocellGeneratorRecipes.java
+++ src/main/java/com/science/gtnl/common/recipe/GTNL/ElectrocellGeneratorRecipes.java
@@ -35,5 +35,37 @@ public class ElectrocellGeneratorRecipes implements IRecipePool {
             .metadata(GENERATOR_EUT, Long.MAX_VALUE)
             .duration(5000)
             .addTo(EGR);
+
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Carbon, 1),
+                GTOreDictUnificator.get(OrePrefixes.plateDouble, Materials.Lead, 2))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Galena, 2))
+            .fluidInputs(
+                Materials.SodiumPersulfate.getFluid(2000))
+            .fluidOutputs(Materials.Hydrogen.getGas(2000))
+            .outputChances(5000)
+            .eut(0)
+            .specialValue(110)
+            .metadata(GENERATOR_EUT, 2560L)
+            .duration(430)
+            .addTo(EGR);
+
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTOreDictUnificator.get(OrePrefixes.plate, Materials.Carbon, 3),
+                GTOreDictUnificator.get(OrePrefixes.plateDouble, Materials.Lead, 4))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Galena, 3))
+            .fluidInputs(
+                Materials.SulfuricAcid.getFluid(6000))
+            .fluidOutputs(Materials.Hydrogen.getGas(6000))
+            .outputChances(7500)
+            .eut(0)
+            .specialValue(110)
+            .metadata(GENERATOR_EUT, 6144L)
+            .duration(430)
+            .addTo(EGR);
     }
 }
+
+
